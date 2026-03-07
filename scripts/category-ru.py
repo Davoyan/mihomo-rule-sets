5c593d157999d1517ae47bc393f4820d6f731ddc
Davoyan <20373889+Davoyan@users.noreply.github.com>
2026-03-07 16:32:50 +0300

    Update transform_keyword to return None
    Add a return statement for None in transform_keyword function.

--- scripts/category-ru.py
+++ scripts/category-ru.py
@@ -22,6 +22,7 @@ def remove_overlaps(domains: set[str]) -> list[str]:
 
 
 def transform_keyword(line: str) -> str | None:
+    return None
     if not line.startswith("keyword:"):
         return None
 

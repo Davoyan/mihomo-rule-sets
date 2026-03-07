77516bb253bca0737ff6cc8a9d87505970096dc8
Davoyan <20373889+Davoyan@users.noreply.github.com>
2026-03-07 15:32:58 +0300

    Update category-ru.py

--- scripts/category-ru.py
+++ scripts/category-ru.py
@@ -55,8 +55,8 @@ def main():
         "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/yandex.list",
         "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/kaspersky.list",
         "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/wildberries.list",
-        "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/refs/geo/geosite/ozon.list",
-        "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/refs/heads/meta/geo/geosite/category-bank-ru.list",
+        "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/ozon.list",
+        "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/category-bank-ru.list",
     ]
 
     ru_domains = set([

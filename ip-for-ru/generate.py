3032bc2613fcae43ef8833b490fac519887079f3
Davoyan <20373889+Davoyan@users.noreply.github.com>
2026-03-26 06:39:46 +0300

    Update generate.py

--- ip-for-ru/generate.py
+++ ip-for-ru/generate.py
@@ -22,7 +22,7 @@ FULL_DOMAIN = ["ya.ru", "yandex.net", "reg.ru", "mail.ru", "cloud.ru", "megafon.
                "donsattv.ru", "mobile-win.ru", "crelcom.ru", "xn--80ahgneri.net", "crimea-com.net", "crimea-com.ru", "ardinvest.net", "redi.su",
                "miranda-media.ru", "realnet.ru", "d-group.online", "mageal.ru", "m3x.org", "liveproxy.ru", "meshnet.su", "mytrinet.ru",
                "bestline.su", "tkmotel.ru", "skymaxsib.ru", "crimea.com", "sevstar.net", "sevtelecom.ru", "ubsnet.ru", "komfort21vek.ru", "avanta-telecom.ru", "reconn.ru"
-               "airee.ru", "rusety.ru", "1city.org", "naukanet.ru"]
+               "airee.ru", "rusety.ru", "1city.org", "naukanet.ru", "ekma.is", "ekma-is.ru"]
 
 
 def iso_from_maxmind(record: dict) -> str | None:

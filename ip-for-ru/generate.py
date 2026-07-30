1c7c2ce3d66d08f7d61eb5adb6dd4e4ae69847da
Davoyan <20373889+Davoyan@users.noreply.github.com>
2026-07-30 20:44:11 +0300

    Add 'timeweb.ru' to the domain list in generate.py

--- ip-for-ru/generate.py
+++ ip-for-ru/generate.py
@@ -24,7 +24,9 @@ FULL_DOMAIN = ["ya.ru", "yandex.net", "reg.ru", "mail.ru", "cloud.ru", "majordom
                "miranda-media.ru", "realnet.ru", "d-group.online", "mageal.ru", "m3x.org", "liveproxy.ru", "meshnet.su", "mytrinet.ru",
                "bestline.su", "tkmotel.ru", "skymaxsib.ru", "crimea.com", "sevstar.net", "sevtelecom.ru", "ubsnet.ru", "komfort21vek.ru", "avanta-telecom.ru", "reconn.ru"
                "airee.ru", "rusety.ru", "1city.org", "naukanet.ru", "ekma.is", "ekma-is.ru", "ugletele.com", "lds.online", "evpanet.com", "maximusnet.ru", "my-trinity.com",
-               "antiddos.solutions", "miran.ru", "spd-mgts.ru", "volnamobile.ru", "yaltanet.ru"]
+               "antiddos.solutions", "miran.ru", "spd-mgts.ru", "volnamobile.ru", "yaltanet.ru", 
+               "timeweb.ru", # Домен у РУ подсетей таймвеба
+              ]
 
 WANTED_AS = [
     # VK AND MAILRU
